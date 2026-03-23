de4a935df562c88cf1f94313a5d787a6fc042522
k <lu24t@proton.me>
2026-03-23 18:20:46 +0100

    Create PlanDeCampagne.py

---
+++ AffichageDeTexte/PlanDeCampagne.py
@@ -0,0 +1,4 @@
+#Dans l'affichage final, afin d'avoir un saut à la ligne on peut soit mettre deux instructions print à la suite, ou remplacer le saut de ligne par '\n'
+
+print("Tout droit tu grimperas,") #ici on remarque un saut de ligne
+print("La clé tu trouveras,\nHabile tu seras,\nQuand tu les porteras,\nEt avec le chef tu reviendras !")
