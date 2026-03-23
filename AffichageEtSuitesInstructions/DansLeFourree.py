200c6c9677e4f9fcc2f5982b8a4cb2737031329e
k <lu24t@proton.me>
2026-03-23 18:31:32 +0100

    Create DansLeFourree.py

---
+++ AffichageEtSuitesInstructions/DansLeFourree.py
@@ -0,0 +1,13 @@
+#Ce que doit faire le programme : diriger un robot dans une grille
+#On doit importer/appeler le robot et ses fonctions
+
+from robot import *
+
+haut() #le robot ne tourne pas, haut() ne signifie pas qu'il se tourne vers la case du haut mais bien qu'il se déplace sur cette case 
+haut()
+haut()
+droite()
+droite()
+bas()
+bas()
+droite()
