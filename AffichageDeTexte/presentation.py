2593150592d5638d2876b6b2e30684cd2e7c476b
k <lu24t@proton.me>
2026-03-23 18:15:41 +0100

    Create presentation.py

---
+++ AffichageDeTexte/presentation.py
@@ -0,0 +1,8 @@
+#Le code doit afficher les infomations dans le bon ordre, salutations, présentation, devise.
+#Ici ce qu'on veut que vous compreniez c'est qu'en python les instructions dans le programme s'éxecutent par ordre d'apparition. 
+#On note aussi qu'au sein de l'instruction print, on met ce que l'on souhaite afficher entre guillemet. 
+#C'est pour cela qu'on utilise des apostrophes pour simuler les guillements dans le programme.
+
+print("Coucou !")
+print("Je m'appelle Camthalion")
+print("Ma devise est 'Parler peu mais parler bien'.")
